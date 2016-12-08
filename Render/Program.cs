a8b12ccda94e3163ecdf7798d04c4762e35aa1a6
Mexahoid <mirramax97@Mail.ru>
2016-12-08 20:13:34 +0300

    Sas

--- Render/Program.cs
+++ Render/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Render
@@ -16,7 +15,7 @@ namespace Render
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
